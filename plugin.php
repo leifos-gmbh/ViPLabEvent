f4c31140157ee8c4505468e5168ab3d3977442a1
smeyer-ilias <smeyer.ilias@gmx.de>
2017-03-29 10:31:33 +0200

    increased plugin version

--- plugin.php
+++ plugin.php
@@ -2,11 +2,11 @@
 // alphanumerical ID of the plugin; never change this
 $id = "viplabevent";
 // code version; must be changed for all code changes
-$version = '1.0';
+$version = '5.2.1';
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "5.1.4";
-$ilias_max_version = "5.1.999";
+$ilias_min_version = "5.2.2";
+$ilias_max_version = "5.2.999";
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Stefan Meyer";
