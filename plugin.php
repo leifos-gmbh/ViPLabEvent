4e4a0aa552f26bb0a16ed84d92c98e1dbf120b43
Per Pascal Grube <pascal.grube@flightmare.net>
2018-04-01 16:36:20 +0200

    set max ilias version to 5.3

--- plugin.php
+++ plugin.php
@@ -6,7 +6,7 @@ $version = '5.2.1';
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "5.2.2";
-$ilias_max_version = "5.2.999";
+$ilias_max_version = "5.3.999";
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Stefan Meyer";
